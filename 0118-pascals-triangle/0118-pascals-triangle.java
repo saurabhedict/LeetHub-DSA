b5490623b56247216391c7253c92c5ddd2f49fa0
Saurabh Tiwari <saurabhedict@gmail.com>
2026-05-05 16:04:03 +0530

    Time: 1 ms (95.47%), Space: 43.7 MB (16.03%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,17 +1,63 @@
+// class Solution {
+//     public List<List<Integer>> generate(int numRows) {
+//      List<List<Integer>> ans = new ArrayList<>(numRows);
+      
+//        for(int i = 0; i<numRows; i++) {
+//         List<Integer> l = new ArrayList<>();
+//         for(int j = 0; j<=i; j++) {
+//           l.add(1);
+//         }
+//         ans.add(l);
+
+//         for(int j = 1; j<i; j++) {
+//            ans.get(i).set(j, ans.get(i-1).get(j) + ans.get(i-1).get(j-1));
+//         }
+//        }
+//        return ans;
+
+//     }
+// }
+
+
+
+
+// class Solution {
+//     public List<List<Integer>> generate(int numRows) {
+//      List<List<Integer>> ans = new ArrayList<>(numRows);
+      
+//        for(int i = 0; i<numRows; i++) {
+//         List<Integer> l = new ArrayList<>();
+//         for(int j = 0; j<=i; j++) {
+//           l.add(1);
+//         }
+//         ans.add(l);
+
+//         for(int j = 1; j<i; j++) {
+//            ans.get(i).set(j, ans.get(i-1).get(j) + ans.get(i-1).get(j-1));
+//         }
+//        }
+//        return ans;
+
+//     }
+// }
+
+
+
 class Solution {
     public List<List<Integer>> generate(int numRows) {
      List<List<Integer>> ans = new ArrayList<>(numRows);
       
        for(int i = 0; i<numRows; i++) {
+
         List<Integer> l = new ArrayList<>();
+
         for(int j = 0; j<=i; j++) {
-          l.add(1);
+         if(j == 0 || j == i) l.add(1);
+         else{
+             l.add(j, ans.get(i-1).get(j) + ans.get(i-1).get(j-1));
+         }
         }
         ans.add(l);
-
-        for(int j = 1; j<i; j++) {
-           ans.get(i).set(j, ans.get(i-1).get(j) + ans.get(i-1).get(j-1));
-        }
        }
        return ans;
 
