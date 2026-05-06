d0542aedc60504e4495dc71112773619c8c7f658
Saurabh Tiwari <saurabhedict@gmail.com>
2026-05-06 17:21:12 +0530

    Time: 1 ms (98.23%), Space: 47.7 MB (43.52%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -24,32 +24,90 @@
 //         }
 
 //     }
-// } //worst method : M1
+// } //worst method : M1 : m*n
+
+
+
+
+
+// class Solution {
+//     public void setZeroes(int[][] arr) {
+//         int r = arr.length; 
+//         int c = arr[0].length;
+//          boolean[] row = new boolean[r];
+//          boolean[] col = new boolean[c];
+
+//         for(int i = 0; i<r; i++){
+//             for(int j = 0; j<c; j++){
+//                 if(arr[i][j] == 0) {
+//                     row[i] = true;
+//                     col[j] = true;
+//                 }
+//             }
+//         }
+
+//         for(int i = 0; i<r; i++){
+//             for(int j = 0; j<c; j++){
+//                 if(row[i] == true || col[j] == true){
+//                     arr[i][j] = 0;
+//                 }
+//             }
+//         }
+
+//     }
+// } //M2 : m+n
+
+
+
 
 
 class Solution {
     public void setZeroes(int[][] arr) {
         int r = arr.length; 
         int c = arr[0].length;
-         boolean[] row = new boolean[r];
-         boolean[] col = new boolean[c];
-
+        boolean r1 = false;
+        boolean c1 = false;
         for(int i = 0; i<r; i++){
             for(int j = 0; j<c; j++){
-                if(arr[i][j] == 0) {
-                    row[i] = true;
-                    col[j] = true;
+                if(i == 0 || j == 0){
+                    if(i == 0 && arr[i][j] == 0) r1 = true;
+                    if(j == 0 && arr[i][j] == 0) c1 = true;
                 }
             }
         }
 
-        for(int i = 0; i<r; i++){
-            for(int j = 0; j<c; j++){
-                if(row[i] == true || col[j] == true){
+        for(int i = 1; i<r; i++){
+            for(int j = 1; j<c; j++){
+                 if(arr[i][j] == 0){
+                    arr[i][0] = 0;
+                    arr[0][j] = 0;
+                 }
+            }
+        }
+
+        for(int i = 1; i<r; i++){
+            for(int j = 1; j<c; j++){
+                if(arr[i][0] == 0 || arr[0][j] == 0){
                     arr[i][j] = 0;
                 }
             }
         }
 
+        if(r1 == true){
+            for(int i = 0; i<1; i++){
+            for(int j = 0; j<c; j++){
+                arr[0][j] = 0;
+            }
+        }
+        }
+
+        if(c1 == true){
+            for(int i = 0; i<r; i++){
+            for(int j = 0; j<1; j++){
+                arr[i][0] = 0;
+            }
+        }
+        }
+
     }
-}
+} //M2 : m+n
