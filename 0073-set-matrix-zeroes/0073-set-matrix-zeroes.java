ab038889155e4ad408c492ba1f90aedac311ce43
Saurabh Tiwari <saurabhedict@gmail.com>
2026-05-06 16:23:46 +0530

    Time: 1 ms (98.23%), Space: 47.5 MB (62.28%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,24 +1,52 @@
+// class Solution {
+//     public void setZeroes(int[][] arr) {
+//         int r = arr.length; 
+//         int c = arr[0].length;
+//         int[][] brr = new int[r][c];
+
+//         for(int i = 0; i<r; i++){
+//             for(int j = 0; j<c; j++){
+//                 brr[i][j] = arr[i][j];
+//             }
+//         }//helper array
+
+//         for(int i = 0; i<r; i++){
+//             for(int j = 0; j<c; j++){
+//                 if(brr[i][j] == 0){
+//                     for(int k = 0; k<c; k++){
+//                         arr[i][k] = 0;
+//                     }
+//                     for(int l = 0; l<r; l++){
+//                         arr[l][j] = 0;
+//                     }
+//                 }
+//             }
+//         }
+
+//     }
+// } //worst method : M1
+
+
 class Solution {
     public void setZeroes(int[][] arr) {
         int r = arr.length; 
         int c = arr[0].length;
-        int[][] brr = new int[r][c];
+         boolean[] row = new boolean[r];
+         boolean[] col = new boolean[c];
 
         for(int i = 0; i<r; i++){
             for(int j = 0; j<c; j++){
-                brr[i][j] = arr[i][j];
+                if(arr[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
             }
-        }//helper array
+        }
 
         for(int i = 0; i<r; i++){
             for(int j = 0; j<c; j++){
-                if(brr[i][j] == 0){
-                    for(int k = 0; k<c; k++){
-                        arr[i][k] = 0;
-                    }
-                    for(int l = 0; l<r; l++){
-                        arr[l][j] = 0;
-                    }
+                if(row[i] == true || col[j] == true){
+                    arr[i][j] = 0;
                 }
             }
         }
