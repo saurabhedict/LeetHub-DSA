e74a291cb945962a3d7dd994d2fb80e79340bb8d
Saurabh Tiwari <saurabhedict@gmail.com>
2026-06-17 13:52:04 +0530

    Time: 5 ms (8.46%), Space: 48.2 MB (6.52%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -19,23 +19,45 @@
 // }
 
 
+// class Solution {
+//     public void moveZeroes(int[] nums) {
+//         int n = nums.length;
+//         //Bubble Sort :
+
+//        int noz = 0;
+//         for(int ele : nums){
+//             if(ele == 0) noz++;
+//         }
+//         for(int x = 0; x<noz; x++){
+//             for(int i = 0; i<n-1-x; i++){
+//                 if(nums[i] == 0){
+//                     int temp = nums[i];
+//                     nums[i] = nums[i+1];
+//                     nums[i+1] = temp;
+//                 }
+//             }
+//         }
+//     }
+// }
+
+
+
 class Solution {
     public void moveZeroes(int[] nums) {
         int n = nums.length;
-        //Bubble Sort :
+        ArrayList<Integer> al = new ArrayList<>();
 
-       int noz = 0;
+        int noz = 0;
         for(int ele : nums){
-            if(ele == 0) noz++;
+            if(ele != 0) al.add(ele);
+            else noz++;
         }
-        for(int x = 0; x<noz; x++){
-            for(int i = 0; i<n-1-x; i++){
-                if(nums[i] == 0){
-                    int temp = nums[i];
-                    nums[i] = nums[i+1];
-                    nums[i+1] = temp;
-                }
-            }
+
+        for(int i = 0; i<noz; i++){
+            al.add(0);
+        }
+        for(int i = 0; i<n; i++){
+            nums[i] = al.get(i);
         }
     }
 }
