31f61844121a554ab96d8f60ed403646c2973b99
Saurabh Tiwari <saurabhedict@gmail.com>
2026-06-17 13:36:34 +0530

    Time: 173 ms (5.2%), Space: 47.9 MB (37.6%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -5,7 +5,7 @@ class Solution {
 
         for(int x = 0; x<n-1; x++){
             boolean flag = true;
-            for(int i = 0; i<n-1; i++){
+            for(int i = 0; i<n-1-x; i++){
                 if(nums[i] == 0){
                     int temp = nums[i];
                     nums[i] = nums[i+1];
