d619bd3e9c8babe4c01a65eb1a5f52b6c3d60e58
Saurabh Tiwari <saurabhedict@gmail.com>
2026-06-17 13:35:55 +0530

    Time: 434 ms (5.2%), Space: 47.5 MB (87.2%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        //Bubble Sort :
+
+        for(int x = 0; x<n-1; x++){
+            boolean flag = true;
+            for(int i = 0; i<n-1; i++){
+                if(nums[i] == 0){
+                    int temp = nums[i];
+                    nums[i] = nums[i+1];
+                    nums[i+1] = temp;
+                    flag = false;
+                }
+            }
+            if(flag == true) break;
+        }
+    }
+}
